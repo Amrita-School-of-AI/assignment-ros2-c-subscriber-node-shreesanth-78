74ef4d46ca2bc72480a0208e647319edfdb3cbf2
shreesanth-78 <shreesanth78official@gmail.com>
2026-02-02 21:55:29 +0530

    Completed ROS2 C++ subscriber node

--- src/subscriber_node.cpp
+++ src/subscriber_node.cpp
@@ -4,27 +4,25 @@
 class SubscriberNode : public rclcpp::Node
 {
 public:
-    SubscriberNode() : Node("subscriber_node")
-    {
-        subscription_ = this->create_subscription<std_msgs::msg::String>(
-            "/chatter", 10,
-            std::bind(&SubscriberNode::topic_callback, this, std::placeholders::_1));
-    }
+  SubscriberNode() : Node("subscriber_node")
+  {
+    subscription_ = this->create_subscription<std_msgs::msg::String>(
+      "/chatter", 
+      10, 
+      [this](std_msgs::msg::String::SharedPtr msg) {
+        RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg->data.c_str());
+      });
+  }
 
 private:
-    void topic_callback(const std_msgs::msg::String::SharedPtr msg)
-    {
-        RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg->data.c_str());
-    }
-
-    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
+  rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
 };
 
-int main(int argc, char **argv)
+int main(int argc, char * argv[])
 {
-    rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<SubscriberNode>());
-    rclcpp::shutdown();
-    return 0;
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<SubscriberNode>());
+  rclcpp::shutdown();
+  return 0;
 }
 
